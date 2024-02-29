6dd410e4dbbb7f3000f264e649a5b11f3d661ca6
Soulou <leo@scalingo.com>
2024-02-29 01:01:50 +0100

    Fix regex for redis

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-REGEX='^([^:]+)://([^:]+):([^@]+)@([^:]+):([^/]+)/([^?]+)(.*)$'
+PG_REGEX='^([^:]+)://([^:]+):([^@]+)@([^:]+):([^/]+)/([^?]+)(.*)$'
+REDIS_REGEX='^([^:]+)://:([^@]+)@([^:]+):([^/]+)$'
 
 if [[ "$DATABASE_URL" ]]; then
-  if [[ "$DATABASE_URL" =~ $REGEX ]]; then
+  if [[ "$DATABASE_URL" =~ $PG_REGEX ]]; then
     export DB_POSTGRESDB_USER=${BASH_REMATCH[2]}
     export DB_POSTGRESDB_PASSWORD=${BASH_REMATCH[3]}
     export DB_POSTGRESDB_HOST=${BASH_REMATCH[4]}
@@ -19,11 +20,11 @@ else
 fi
 
 if [[ "$REDIS_URL" ]]; then
-  if [[ "$REDIS_URL" =~ $REGEX ]]; then
+  if [[ "$REDIS_URL" =~ $REDIS_REGEX ]]; then
     export EXECUTIONS_MODE="${EXECUTIONS_MODE:-queue}"
-    export QUEUE_BULL_REDIS_HOST=${BASH_REMATCH[4]}
-    export QUEUE_BULL_REDIS_PORT=${BASH_REMATCH[5]}
-    export QUEUE_BULL_REDIS_PASSWORD=${BASH_REMATCH[3]}
+    export QUEUE_BULL_REDIS_HOST=${BASH_REMATCH[3]}
+    export QUEUE_BULL_REDIS_PORT=${BASH_REMATCH[4]}
+    export QUEUE_BULL_REDIS_PASSWORD=${BASH_REMATCH[2]}
   else
     echo " !     Invalid REDIS_URL! Please use the URL provided by the Scalingo addon.">&2
     exit 1
